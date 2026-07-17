0f7abb259101327ce187f60005234c9b0ff88646
Natàlia <natalia@plecmedia.com>
2026-07-17 15:25:44 +0200

    Update network_mqtt.cpp

--- ns_device/esp32_firmware/network_mqtt.cpp
+++ ns_device/esp32_firmware/network_mqtt.cpp
@@ -1,1 +1,22 @@
+#include "network_mqtt.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_mqtt() {
+    WiFi.begin("SSID", "PASSWORD");
+    while (WiFi.status() != WL_CONNECTED) delay(500);
+
+    client.setServer("192.168.1.50", 1883);
+    while (!client.connected()) {
+        client.connect("ESP32_AUDIO");
+        delay(500);
+    }
+}
+
+void mqtt_publish(const char *topic, uint8_t *data, int len) {
+    client.publish(topic, data, len);
+}
 
