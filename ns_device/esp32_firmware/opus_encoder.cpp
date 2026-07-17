f86bc3ad0cf45a1ca7ce9090d29c6878996656df
Natàlia <natalia@plecmedia.com>
2026-07-17 15:25:12 +0200

    Update opus_encoder.cpp

--- ns_device/esp32_firmware/opus_encoder.cpp
+++ ns_device/esp32_firmware/opus_encoder.cpp
@@ -1,1 +1,15 @@
+#include "opus_encoder.h"
+#include "opus.h"
+
+OpusEncoder *encoder;
+
+void setup_opus() {
+    int error;
+    encoder = opus_encoder_create(16000, 1, OPUS_APPLICATION_AUDIO, &error);
+    opus_encoder_ctl(encoder, OPUS_SET_BITRATE(24000));
+}
+
+int encode_opus(int16_t *pcm, uint8_t *out, int max_len) {
+    return opus_encode(encoder, pcm, 16000, out, max_len);
+}
 
