1d2f27a34a23a1c4a74c93830a07503700ed6063
Natàlia <natalia@plecmedia.com>
2026-07-17 15:24:36 +0200

    Update i2s_config.cpp

--- ns_device/esp32_firmware/i2s_config.cpp
+++ ns_device/esp32_firmware/i2s_config.cpp
@@ -1,1 +1,26 @@
+#include "i2s_config.h"
+#include <driver/i2s.h>
+
+void setup_i2s() {
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = 16000,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 4,
+        .dma_buf_len = 512
+    };
+
+    i2s_pin_config_t pin_config = {
+        .ws_io_num = 25,
+        .bck_io_num = 26,
+        .data_in_num = 22,
+        .data_out_num = I2S_PIN_NO_CHANGE
+    };
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+}
 
