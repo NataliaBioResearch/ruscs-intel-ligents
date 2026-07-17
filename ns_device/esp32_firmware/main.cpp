c821b1ed9865be0334dba818d9c30ed8997a5fc0
Natàlia <natalia@plecmedia.com>
2026-07-17 15:24:22 +0200

    Update main.cpp

--- ns_device/esp32_firmware/main.cpp
+++ ns_device/esp32_firmware/main.cpp
@@ -1,1 +1,28 @@
+#include "i2s_config.h"
+#include "opus_encoder.h"
+#include "network_mqtt.h"
+
+uint8_t opus_buffer[4000];
+
+void setup() {
+    setup_i2s();
+    setup_opus();
+    setup_mqtt();
+}
+
+void loop() {
+    int16_t pcm_buffer[16000];
+    size_t bytes_read;
+
+    // Captura 1 segon d'àudio
+    i2s_read(I2S_NUM_0, pcm_buffer, sizeof(pcm_buffer), &bytes_read, portMAX_DELAY);
+
+    // Codifica a Opus
+    int opus_len = encode_opus(pcm_buffer, opus_buffer, sizeof(opus_buffer));
+
+    // Envia via MQTT
+    mqtt_publish("rusc/audio", opus_buffer, opus_len);
+
+    delay(10);
+}
 
